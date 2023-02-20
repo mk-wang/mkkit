8639e3058159f34f4c444ea7d1456862d66f3c16
MK <wangmingkun@drojian.dev>
2023-02-20 21:30:34 +0800

    NSMutableURLRequest : addBasicAuth

--- Core/Common/Ext/UIKit/URLRequestExt.swift
+++ Core/Common/Ext/UIKit/URLRequestExt.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
-extension URLRequest {
+public extension URLRequest {
+    var authorizationHeader: String? {
+        value(forHTTPHeaderField: "Authorization") as? String
+    }
+
     mutating func addBasicAuth(userName: String, password: String) {
+        let loginStr = "\(userName):\(password)"
+        if let base64Str = loginStr.utf8Base64Str {
+            setValue("Basic \(base64Str)", forHTTPHeaderField: "Authorization")
+        }
+    }
+}
+
+public extension NSMutableURLRequest {
+    func addBasicAuth(userName: String, password: String) {
         let loginStr = "\(userName):\(password)"
         if let base64Str = loginStr.utf8Base64Str {
             setValue("Basic \(base64Str)", forHTTPHeaderField: "Authorization")
