7307086a100ca37c5b3033bb8280fcc2683c9d2e
MK <wangmingkun@drojian.dev>
2023-02-22 13:41:07 +0800

    YXCALayerWrapView  add gradientLayerWrap

--- Core/ObjC/Utils/YXCALayerWrapView.swift
+++ Core/ObjC/Utils/YXCALayerWrapView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// MARK: - YXCALayerWrapView
+
 open class YXCALayerWrapView: UIView {
     public var wrapLayer: CALayer? {
         didSet {
@@ -21,5 +23,35 @@ open class YXCALayerWrapView: UIView {
     override open func layoutSubviews() {
         super.layoutSubviews()
         wrapLayer?.frame = bounds
+    }
+}
+
+public extension YXCALayerWrapView {
+    class func gradientLayerWrap(colors: [UIColor],
+                                 start: CGPoint,
+                                 end: CGPoint,
+                                 locations: [CGFloat]? = nil)
+        -> YXCALayerWrapView
+    {
+        let box = YXCALayerWrapView()
+        let bgLayer = CAGradientLayer()
+        bgLayer.colors = colors.map(\.cgColor)
+        bgLayer.startPoint = start
+        bgLayer.endPoint = end
+        if let locations {
+            bgLayer.locations = locations.map { NSNumber(value: $0) }
+        }
+        box.wrapLayer = bgLayer
+        return box
+    }
+
+    class func verticalGradientLayer(colors: [UIColor],
+                                     locations: [CGFloat]? = nil)
+        -> YXCALayerWrapView
+    {
+        Self.gradientLayerWrap(colors: colors,
+                               start: .init(0.5, 0),
+                               end: .init(0.5, 1),
+                               locations: locations)
     }
 }
