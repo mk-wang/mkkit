1cf9ebe766d4e2726051059c941576b8e9fcd5b1
MK <wangmingkun@drojian.dev>
2023-07-27 14:34:03 +0800

    add CombineInfo to NSObject

--- Core/Common/Ext/ObjC/NSObjectCombile.swift
+++ Core/Common/Ext/ObjC/NSObjectCombile.swift
@@ -1,27 +1,27 @@
-//
-//  NSObjectCombile.swift
-//
-//
-//  Created by MK on 2022/3/21.
-//
 
-// import Foundation
-// import OpenCombine
-//
-// extension NSObject {
-//    var cancellableSet: NSMutableSet {
-//        if let set = getAssociatedObject(&AssociatedKeys.kCancelSet) as? NSMutableSet {
-//            return set
-//        }
-//
-//        let set = NSMutableSet()
-//        setAssociatedObject(&AssociatedKeys.kCancelSet, set)
-//        return set
-//    }
-// }
-//
-//// MARK: - AssociatedKeys
-//
-// private enum AssociatedKeys {
-//    static var kCancelSet = 0
-// }
+import Foundation
+import OpenCombine
+
+// MARK: - CombineInfo
+
+public final class CombineInfo {
+    public var cancellableSet = Set<AnyCancellable>()
+}
+
+public extension NSObject {
+    var combineInfo: CombineInfo {
+        if let info = getAssociatedObject(&AssociatedKeys.kCombineInfo) as? CombineInfo {
+            return info
+        }
+
+        let info = CombineInfo()
+        setAssociatedObject(&AssociatedKeys.kCombineInfo, info)
+        return info
+    }
+}
+
+// MARK: - AssociatedKeys
+
+private enum AssociatedKeys {
+    static var kCombineInfo = 0
+}
