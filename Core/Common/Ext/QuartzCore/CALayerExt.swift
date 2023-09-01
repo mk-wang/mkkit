35d79a98e850eb8d3c7d5f1ef6151eebf0b3ef18
MK <wangmingkun@drojian.dev>
2023-09-01 22:48:02 +0800

    add  resume / pause to calayer

--- Core/Common/Ext/QuartzCore/CALayerExt.swift
+++ Core/Common/Ext/QuartzCore/CALayerExt.swift
@@ -71,6 +71,23 @@ extension CALayer {
     }
 }
 
+public extension CALayer {
+    func pause() {
+        let pausedTime: CFTimeInterval = self.convertTime(CACurrentMediaTime(), from: nil)
+        self.speed = 0.0
+        self.timeOffset = pausedTime
+    }
+
+    func resume() {
+        let pausedTime: CFTimeInterval = self.timeOffset
+        self.speed = 1.0
+        self.timeOffset = 0.0
+        self.beginTime = 0.0
+        let timeSincePause: CFTimeInterval = self.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        self.beginTime = timeSincePause
+    }
+}
+
 //  Pause animations of layer tree
 //
 //  Technical Q&A QA1673:
