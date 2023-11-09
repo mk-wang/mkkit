ec8d6aa07bd58f728e8bd74d3b4647c03cfbc621
MK <wangmingkun@drojian.dev>
2023-11-09 12:45:22 +0800

    add DisplayLink

---
+++ Core/Common/Utils/DisplayLink.swift
@@ -0,0 +1,83 @@
+//
+//  DisplayLink.swift
+//  MKKit
+//
+//  Created by MK on 2023/11/9.
+//
+
+import Foundation
+
+open class DisplayLink {
+    public let callback: VoidFunction2<DisplayLink, TimeInterval>
+    public let fps: Int
+
+    private var displayLink: CADisplayLink?
+    private var nextFireTime: TimeInterval?
+
+    public init(fps: Int = 60,
+                callback: @escaping VoidFunction2<DisplayLink, TimeInterval>)
+    {
+        self.fps = fps
+        self.callback = callback
+    }
+
+    deinit {
+        stop()
+    }
+
+    open var isRunning: Bool {
+        guard let displayLink else {
+            return false
+        }
+        return !displayLink.isPaused
+    }
+
+    open var isPaused: Bool {
+        displayLink?.isPaused ?? false
+    }
+
+    open var duration: TimeInterval {
+        var value: TimeInterval = 0
+        if let nextFireTime {
+            value = nextFireTime - CACurrentMediaTime()
+            if value < 0 {
+                value = displayLink?.duration ?? 0
+            }
+        }
+        return value
+    }
+
+    open func pause() {
+        displayLink?.isPaused = true
+    }
+
+    open func resume() {
+        displayLink?.isPaused = false
+    }
+
+    open func start() {
+        stop()
+
+        let proxy = WeakProxy(target: self)
+        let displayLink = CADisplayLink(target: proxy, selector: #selector(displayLinkTick))
+        if #available(iOS 15.0, *) {
+            let preferred = Float(fps)
+            displayLink.preferredFrameRateRange = .init(minimum: preferred, maximum: preferred, preferred: preferred)
+        } else {
+            displayLink.preferredFramesPerSecond = fps
+        }
+        displayLink.add(to: RunLoop.main, forMode: .common)
+
+        self.displayLink = displayLink
+    }
+
+    open func stop() {
+        displayLink?.invalidate()
+        displayLink = nil
+    }
+
+    @objc private func displayLinkTick(_ displayLink: CADisplayLink) {
+        nextFireTime = displayLink.targetTimestamp
+        callback(self, displayLink.targetTimestamp - displayLink.timestamp)
+    }
+}
