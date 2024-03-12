7d221b59ca1c1c98927f977ec617ff77a715f357
MK <wangmingkun@drojian.dev>
2024-03-12 20:02:53 +0800

    add LocalNetworkManager

---
+++ Core/Common/Utils/LocalNetworkManager.swift
@@ -0,0 +1,134 @@
+//
+//  LocalNetworkManager.swift
+//  TVRemote
+//
+//  Created by MK on 2024/3/12.
+//  https://www.jianshu.com/p/0daf226a1536
+
+import Foundation
+import Network
+import OpenCombine
+
+// MARK: - LocalNetworkManager
+
+open class LocalNetworkManager: NSObject {
+    public enum AuthState {
+        case unkown
+        case checking
+        case denied
+        case authed
+
+        public var isDone: Bool {
+            self == .denied || self == .authed
+        }
+    }
+
+    private let stateSubject: CurrentValueSubject<AuthState, Never> = .init(LocalNetworkManager.needAuth ? .authed : .unkown)
+    public lazy var statePublisher = stateSubject.eraseToAnyPublisher()
+
+    private(set) var state: AuthState {
+        get {
+            stateSubject.value
+        }
+        set {
+            if stateSubject.value != newValue {
+                stateSubject.value = newValue
+            }
+        }
+    }
+
+    private var netService: NetService?
+    private var _browser: AnyObject?
+
+    public func requestAuthorization() {
+        guard Self.needAuth, #available(iOS 14, *) else {
+            state = .authed
+            return
+        }
+        doAuthorization()
+    }
+
+    static var needAuth: Bool {
+        #if targetEnvironment(simulator)
+            return false
+        #endif
+
+        if #available(iOS 14, *) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+private extension LocalNetworkManager {
+    var browser: NWBrowser? {
+        get {
+            _browser as? NWBrowser
+        }
+        set {
+            _browser = newValue
+        }
+    }
+
+    func doAuthorization() {
+        guard state != .checking else {
+            return
+        }
+
+        state = .checking
+
+        // Create parameters, and allow browsing over peer-to-peer link.
+        let parameters = NWParameters()
+        parameters.includePeerToPeer = true
+
+        // Browse for a custom service type.
+        let browser = NWBrowser(for: .bonjour(type: "_bonjour._tcp", domain: nil), using: parameters)
+        browser.stateUpdateHandler = { [weak self] newState in
+            guard let self else {
+                return
+            }
+
+            switch newState {
+            case let .failed(error):
+                appLogger.debug("localNetwork fail \(error)")
+            case .cancelled,
+                 .ready:
+                break
+            case let .waiting(error):
+                appLogger.debug("localNetwork denied \(error)")
+                stop()
+                state = .denied
+            default:
+                break
+            }
+        }
+
+        let netService = NetService(domain: "local.", type: "_inston-play._tcp.", name: "LocalNetworkPrivacy", port: 1100)
+        netService.delegate = self
+
+        browser.start(queue: .main)
+        netService.publish()
+        // netService.schedule(in: .main, forMode: .common)
+
+        self.browser = browser
+        self.netService = netService
+    }
+
+    func stop() {
+        browser?.cancel()
+        browser = nil
+        netService?.stop()
+        netService = nil
+    }
+}
+
+// MARK: NetServiceDelegate
+
+@available(iOS 14.0, *) extension LocalNetworkManager: NetServiceDelegate {
+    public func netServiceDidPublish(_: NetService) {
+        stop()
+        state = .authed
+    }
+}
