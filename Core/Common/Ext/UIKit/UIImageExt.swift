6d51eaefefa27cfb2b540a5d50505aa0850edce5
MK <wangmingkun@drojian.dev>
2024-05-14 16:23:54 +0800

    update

--- Core/Common/Ext/UIKit/UIImageExt.swift
+++ Core/Common/Ext/UIKit/UIImageExt.swift
@@ -91,10 +91,11 @@ public extension UIImage {
 
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
 
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = scale
-        format.opaque = opaque
         #if !os(watchOS)
+            let format = UIGraphicsImageRendererFormat()
+            format.scale = scale
+            format.opaque = opaque
+
             return UIGraphicsImageRenderer(size: size, format: format).image { [weak self] _ in
                 self?.draw(in: rect)
             }
