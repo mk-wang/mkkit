3352a37aa2652c591f8db4ab6f761f65d1bfd7ce
MK <wangmingkun@drojian.dev>
2024-06-06 21:42:26 +0800

    add TowLabelBox

---
+++ Core/Common/UI/TowLabelBox.swift
@@ -0,0 +1,54 @@
+//
+//  TowLabelBox.swift
+//
+//  Created by MK on 2023/9/13.
+//
+
+import MKKit
+import UIKit
+
+open class TowLabelBox: MKBaseView {
+    public struct Config {
+        let topStyle: TextViewStyle
+        let bottomStyle: TextViewStyle
+        let yPad: CGFloat
+
+        public init(topStyle: TextViewStyle, bottomStyle: TextViewStyle, yPad: CGFloat = 0) {
+            self.topStyle = topStyle
+            self.bottomStyle = bottomStyle
+            self.yPad = yPad
+        }
+    }
+
+    public let topLbl: UILabel
+    public let bottomLbl: UILabel
+    public let config: Config
+
+    public let layoutBloack: ((TowLabelBox, UILabel, UILabel) -> Void)?
+
+    public init(frame: CGRect, config: Config, layoutBloack: ((TowLabelBox, UILabel, UILabel) -> Void)? = nil) {
+        self.config = config
+        self.layoutBloack = layoutBloack
+        topLbl = .init(text: "", style: config.topStyle)
+        bottomLbl = .init(text: "", style: config.bottomStyle)
+        super.init(frame: frame)
+    }
+
+    override open func readyToLayout() {
+        if let layoutBloack {
+            layoutBloack(self, topLbl, bottomLbl)
+        } else {
+            addSnpStackSubviews(.vertical,
+                                builders: [
+                                    .view(topLbl, crossEdgesInset: 0),
+                                    .space(config.yPad),
+                                    .view(bottomLbl, crossEdgesInset: 0),
+                                ])
+        }
+    }
+
+    open func reset() {
+        topLbl.text = nil
+        bottomLbl.text = nil
+    }
+}
