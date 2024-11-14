a8c87b183649a821117c0b7a78a126002d9fbfc4
MK <wangmingkun@drojian.dev>
2024-11-15 02:24:12 +0800

    update TrackInfo

--- Core/Common/Utils/Tracker.swift
+++ Core/Common/Utils/Tracker.swift
@@ -48,22 +48,15 @@ public enum TrackType: Int8 {
 
 // MARK: - TrackInfo
 
-public class TrackInfo {
-    public fileprivate(set) weak var target: NSObject?
-    public let id: String?
-    public let exposure: VoidFunction1<TrackInfo>?
+public struct TrackInfo {
+    public let checker: VoidFunction2<UIView, UIEdgeInsets>
 
-    public init(id: String? = nil,
-                exposure: VoidFunction1<TrackInfo>? = nil)
-    {
-        self.id = id
-        self.exposure = exposure
-    }
-
-    public func checkExposure() {
-        exposure?(self)
+    public init(checker: @escaping VoidFunction2<UIView, UIEdgeInsets>) {
+        self.checker = checker
     }
 }
+
+// MARK: - TrackInfo
 
 public extension NSObject {
     var trackInfo: TrackInfo? {
@@ -71,7 +64,6 @@ public extension NSObject {
             getAssociatedObject(&AssociatedKeys.kTrackInfo) as? TrackInfo
         }
         set {
-            newValue?.target = self
             setAssociatedObject(&AssociatedKeys.kTrackInfo, newValue)
         }
     }
@@ -81,6 +73,59 @@ public extension NSObject {
                                   type: NSMutableSet.self,
                                   builder: { .init() })
     }
+
+    private var trackMap: NSMutableDictionary {
+        getOrMakeAssociatedObject(&AssociatedKeys.kTrackMap,
+                                  type: NSMutableDictionary.self,
+                                  builder: { .init() })
+    }
+}
+
+extension NSObject {
+    @objc open func cleanTrackSet() {
+        trackSet.removeAllObjects()
+
+        for value in trackMap.allValues {
+            (value as? WeakReference)?.reference?.cleanTrackSet()
+        }
+    }
+
+    @objc open func hasTracked(_ value: AnyHashable) -> Bool {
+        if trackSet.contains(value) {
+            return true
+        }
+        return trackMap.allKeys.first { ($0 as? AnyHashable) == value } != nil
+    }
+
+    @objc open func addTrack(_ value: AnyHashable, object: NSObject?) {
+        if let object {
+            trackMap[value] = WeakReference(reference: object)
+        } else {
+            trackMap[value] = Self.nullObject
+        }
+    }
+
+    private static let nullObject = NSNull()
+}
+
+extension UIView {
+    @objc open func checkExposure(outBox: UIView,
+                                  inset: UIEdgeInsets = .zero)
+    {
+        trackInfo?.checker(outBox, inset)
+    }
+
+    @objc open func checkExposure(outBox: UIView,
+                                  inset: UIEdgeInsets = .zero,
+                                  checker: ValueBuilder1<Bool, CGSize>,
+                                  exposure: VoidFunction1<UIView>)
+    {
+        let size = outBox.visibleRect(of: self, inset: inset).size
+        let canExposure = checker(size)
+        if canExposure {
+            exposure(self)
+        }
+    }
 }
 
 // MARK: - AssociatedKeys
@@ -88,4 +133,5 @@ public extension NSObject {
 private enum AssociatedKeys {
     static var kTrackInfo = 0
     static var kTrackSet = 0
+    static var kTrackMap = 0
 }
