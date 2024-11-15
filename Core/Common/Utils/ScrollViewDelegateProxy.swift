b99db3626f01a0f9376b5ea62277d6b5c0cb0158
MK <wangmingkun@drojian.dev>
2024-11-15 20:43:54 +0800

    ScrollViewDelegateProxy add addition delegates

--- Core/Common/Utils/ScrollViewDelegateProxy.swift
+++ Core/Common/Utils/ScrollViewDelegateProxy.swift
@@ -24,9 +24,27 @@ open class ScrollViewDelegateProxy: NSObject, UIScrollViewDelegate {
     private let offsetSubject: PassthroughSubject<CGPoint, Never> = .init()
     public lazy var offsetPublisher = offsetSubject.eraseToAnyPublisher()
 
-    public init(target: ScrollViewDelegateProxyProtocol?) {
+    private var delegates: [WeakReference]
+
+    public init(target: ScrollViewDelegateProxyProtocol?,
+                delegate: UIScrollViewDelegate? = nil)
+    {
         self.target = target
+        self.delegates = [target, delegate].compactMap {
+            $0 == nil ? nil : .init(reference: $0!)
+        }
         super.init()
+    }
+
+    open func addDelegate(_ delegate: UIScrollViewDelegate?) {
+        if let delegate {
+            delegates.append(.init(reference: delegate))
+        }
+    }
+
+    open func removeDelegate(_ delegate: UIScrollViewDelegate?) {
+        guard let delegate else { return }
+        delegates.removeAll { $0.reference === delegate }
     }
 
     // MARK: - Message Forwarding
@@ -35,26 +53,30 @@ open class ScrollViewDelegateProxy: NSObject, UIScrollViewDelegate {
         if super.responds(to: aSelector) {
             return true
         }
-        return target?.responds(to: aSelector) ?? false
+
+        return findDelegate(for: aSelector) != nil
     }
 
     override public func forwardingTarget(for aSelector: Selector!) -> Any? {
-        if target?.responds(to: aSelector) == true {
-            return target
-        }
-        return super.forwardingTarget(for: aSelector)
+        findDelegate(for: aSelector) ?? super.forwardingTarget(for: aSelector)
     }
 
     public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         if scrollView.isStopped {
             scrollViewOnScrollEnd(scrollView)
         }
-        target?.scrollViewDidEndDecelerating?(scrollView)
+
+        if let delegate = findDelegate(for: #selector(UIScrollViewDelegate.scrollViewDidEndDecelerating(_:))) {
+            delegate.scrollViewDidEndDecelerating?(scrollView)
+        }
     }
 
     public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
         scrollViewOnScrollEnd(scrollView)
-        target?.scrollViewDidEndScrollingAnimation?(scrollView)
+
+        if let delegate = findDelegate(for: #selector(UIScrollViewDelegate.scrollViewDidEndScrollingAnimation(_:))) {
+            delegate.scrollViewDidEndScrollingAnimation?(scrollView)
+        }
     }
 
     public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
@@ -62,16 +84,25 @@ open class ScrollViewDelegateProxy: NSObject, UIScrollViewDelegate {
             scrollViewOnScrollEnd(scrollView)
         }
 
-        target?.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
+        if let delegate = findDelegate(for: #selector(UIScrollViewDelegate.scrollViewDidEndDragging(_:willDecelerate:))) {
+            delegate.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
+        }
     }
 
-    public func scrollViewDidZoom(_ scrollView: UIScrollView) {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let delegate = findDelegate(for: #selector(UIScrollViewDelegate.scrollViewDidScroll(_:))) {
+            delegate.scrollViewDidScroll?(scrollView)
+        }
         offsetSubject.send(scrollView.contentOffset)
-        target?.scrollViewDidZoom?(scrollView)
     }
 
     open func scrollViewOnScrollEnd(_ scrollView: UIScrollView) {
         target?.scrollViewOnScrollEnd?(scrollView)
+    }
+
+    private func findDelegate(for selector: Selector) -> UIScrollViewDelegate? {
+        delegates.compactMap { $0.reference as? UIScrollViewDelegate }
+            .first { $0.responds(to: selector) }
     }
 }
 
