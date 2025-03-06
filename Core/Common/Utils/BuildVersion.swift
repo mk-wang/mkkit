38c884288c3326d27bdac73a56e7a5fcaf2f4e97
MK <wangmingkun@drojian.dev>
2025-03-06 19:26:22 +0800

    improve BuildVersion

--- Core/Common/Utils/BuildVersion.swift
+++ Core/Common/Utils/BuildVersion.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-public struct BuildVersion: Comparable, CustomStringConvertible {
+// MARK: - BuildVersion
+
+public struct BuildVersion {
     static let maxComponents = 4
 
     public let components: [Int]
@@ -25,7 +27,11 @@ public struct BuildVersion: Comparable, CustomStringConvertible {
 
         components = parsedComponents
     }
+}
 
+// MARK: Comparable
+
+extension BuildVersion: Comparable {
     public static func < (lhs: BuildVersion, rhs: BuildVersion) -> Bool {
         for (left, right) in zip(lhs.components, rhs.components) {
             if left < right {
@@ -40,8 +46,16 @@ public struct BuildVersion: Comparable, CustomStringConvertible {
     public static func == (lhs: BuildVersion, rhs: BuildVersion) -> Bool {
         lhs.components == rhs.components
     }
+}
 
+// MARK: CustomStringConvertible
+
+extension BuildVersion: CustomStringConvertible {
     public var description: String {
         components.map { String($0) }.joined(separator: ".")
     }
 }
+
+// MARK: Codable
+
+extension BuildVersion: Codable {}
