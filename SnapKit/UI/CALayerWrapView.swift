ee91788510c57848f52469b1ddacd072f1e110e0
MK <wangmingkun@drojian.dev>
2025-06-09 16:00:05 +0800

    Add gradient support to CALayerWrapView and UIView extensions
    - Introduced `GradientDirection` enum to define various gradient directions with corresponding start and end points.
    - Added `gradient` method to `CALayerWrapView` for creating gradient layers based on specified direction and colors.
    - Implemented `addGradient` method in `UIView` to apply gradient colors, enhancing visual customization options.
    - Improved code organization and readability by encapsulating gradient logic within dedicated methods and extensions.

--- SnapKit/UI/CALayerWrapView.swift
+++ SnapKit/UI/CALayerWrapView.swift
@@ -86,3 +86,100 @@ public extension CALayerWrapView {
         }
     }
 }
+
+// MARK: - GradientDirection
+
+public enum GradientDirection {
+    case horizontal
+    case ltrHorizontal
+    case vertical
+    case diagonal
+    case reverseDiagonal
+
+    public var startPoint: CGPoint {
+        switch self {
+        case .horizontal:
+            Lang.current.isRTL ? CGPoint(x: 1.0, y: 0.5) : CGPoint(x: 0.0, y: 0.5)
+        case .ltrHorizontal:
+            CGPoint(x: 0.0, y: 0.5)
+        case .vertical:
+            CGPoint(x: 0.5, y: 0.0)
+        case .diagonal:
+            CGPoint(x: 0.0, y: 0.0)
+        case .reverseDiagonal:
+            CGPoint(x: 1.0, y: 0.0)
+        }
+    }
+
+    public var endPoint: CGPoint {
+        switch self {
+        case .horizontal:
+            Lang.current.isRTL ? CGPoint(x: 0.0, y: 0.5) : CGPoint(x: 1.0, y: 0.5)
+        case .ltrHorizontal:
+            CGPoint(x: 1.0, y: 0.5)
+        case .vertical:
+            CGPoint(x: 0.5, y: 1.0)
+        case .diagonal:
+            CGPoint(x: 1.0, y: 1.0)
+        case .reverseDiagonal:
+            CGPoint(x: 0.0, y: 1.0)
+        }
+    }
+}
+
+public extension CALayerWrapView {
+    class func gradient(direction: GradientDirection,
+                        colors: [UIColor],
+                        locations: [CGFloat]? = nil)
+        -> CALayerWrapView
+    {
+        gradientLayer(colors: colors,
+                      start: direction.startPoint,
+                      end: direction.endPoint,
+                      locations: locations)
+    }
+}
+
+public extension UIView {
+    // MARK: - Apply Text Gradient
+
+    /// Apply gradient color
+    /// - Parameters:
+    ///   - colors: Array of UIColors for the gradient
+    ///   - direction: Direction of the gradient
+    ///   - locations: Optional locations for the colors (0.0 to 1.0)
+    func addGradient(direction: GradientDirection = .horizontal,
+                     colors: [UIColor],
+                     locations: [CGFloat]? = nil)
+    {
+        // Create gradient layer
+        let gradientLayer = CALayerWrapView.gradient(
+            direction: direction,
+            colors: colors,
+            locations: locations
+        )
+
+        gradientLayer.addSnpEdgesToSuper()
+
+        // Create mask
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            UIGraphicsEndImageContext()
+            return
+        }
+
+        layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        if let image {
+            let maskLayer = CALayer()
+            maskLayer.frame = bounds
+            maskLayer.contents = image.cgImage
+            gradientLayer.layer.mask = maskLayer
+
+            // Add gradient
+            insertSnpSubview(gradientLayer, at: 0)
+        }
+    }
+}
